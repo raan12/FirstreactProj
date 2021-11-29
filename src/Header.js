084b72e7dcedc8081ea2814e1d1efcffed42148f
diptanshu <diptanshu10@gmail.com>
2021-11-29 15:36:50 -0500

    s
    s

--- src/Header.js
+++ src/Header.js
@@ -42,10 +42,10 @@ export default function Header() {
 
 
             <Router>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3 shadow-lg" style={{ height: '84px' }}>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3 shadow-lg" style={{ minHeight: '84px' }}>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
+                    <div className="collapse navbar-collapse d-block d-md-flex" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto d-block d-md-flex">
                             <li className="nav-item">
                                 <NavLink exact className="nav-link" activeClassName="active" to="/">Home</NavLink>
                             </li>
